7eea3a3b99834bf9556d4938ec36b4b933af0884
xiomarariverav <XIomara1429@gmail.com>
2026-05-15 09:54:35 -0400

    Initial commit of part 2 workshop4

---
+++ src/main/java/org/example/Contract.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Contract {
+
+    private String contractDate;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+}
