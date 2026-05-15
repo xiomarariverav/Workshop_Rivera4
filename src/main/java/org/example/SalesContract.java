805c8314b5faf310b7cb739d09dccb1bc59d313a
xiomarariverav <XIomara1429@gmail.com>
2026-05-15 10:14:14 -0400

    Started SalesContract

--- src/main/java/org/example/SalesContract.java
+++ src/main/java/org/example/SalesContract.java
@@ -1,4 +1,28 @@
 package org.example;
 
-public class SalesContract {
+public class SalesContract extends Contract{
+
+    private boolean finance;
+    public SalesContract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold, boolean finance) {
+        super(contractDate, customerName, customerEmail, vehicleSold);
+        this.finance = finance;
+    }
+        public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
+
+
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return 0;
+    }
 }
