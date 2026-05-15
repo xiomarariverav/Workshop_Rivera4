8c22236cafbd34ea5a23deafe2b166e664519d5a
xiomarariverav <XIomara1429@gmail.com>
2026-05-15 11:33:38 -0400

    Started LeaseContract

--- src/main/java/org/example/LeaseContract.java
+++ src/main/java/org/example/LeaseContract.java
@@ -1,4 +1,31 @@
 package org.example;
 
-public class LeaseContract {
+import java.math.BigDecimal;
+
+public class LeaseContract extends Contract {
+
+    public LeaseContract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold) {
+        super(contractDate, customerName, customerEmail, vehicleSold);
+    }
+
+    //expected ending value is 50% of the vehicle price
+    public BigDecimal getExpectedEndingValue() {
+        return getVehicleSold().getPrice()
+                .multiply(BigDecimal.valueOf(0.50));
+    }
+//lease fee is 7% of the vehicle price.
+    public BigDecimal getLeaseFee() {
+        return getVehicleSold().getPrice()
+                .multiply(BigDecimal.valueOf(0.07));
+    }
+
+    @Override
+    public BigDecimal getTotalPrice() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getMonthlyPayment() {
+        return null;
+    }
 }
