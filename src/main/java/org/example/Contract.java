cbac254eec5e8f017b707361d71f5e6b13165781
xiomarariverav <XIomara1429@gmail.com>
2026-05-15 10:01:39 -0400

    abstract parent class(contract)

--- src/main/java/org/example/Contract.java
+++ src/main/java/org/example/Contract.java
@@ -1,10 +1,55 @@
 package org.example;
 
-public class Contract {
+public abstract class Contract {
 
     private String contractDate;
     private String customerName;
     private String customerEmail;
     private Vehicle vehicleSold;
 
+    public Contract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.contractDate = contractDate;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+        public String getContractDate() {
+        return contractDate;
+    }
+
+    public void setContractDate(String contractDate) {
+        this.contractDate = contractDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+//Every contract must have these methods but each child class will calculate them differently
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
 }
+
+
+
