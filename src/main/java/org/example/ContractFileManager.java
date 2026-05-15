d8d937716d2262c9b8ef6792c81a231c817752f4
xiomarariverav <XIomara1429@gmail.com>
2026-05-15 11:56:49 -0400

    Started ContractFileManager

---
+++ src/main/java/org/example/ContractFileManager.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ContractFileManager {
+    private static final String filePath = "src/main/resources/contracts.csv";
+
+    public void saveContract(Contract contract) {
+
+    }
+}
+
